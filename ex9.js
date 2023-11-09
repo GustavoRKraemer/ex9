7c907c54c123a0d61d11d5a92face092e2ef9dcd
GustavoRKraemer <130578547+GustavoRKraemer@users.noreply.github.com>
2023-11-09 08:47:32 -0300

    Add files via upload

---
+++ ex9.js
@@ -0,0 +1,16 @@
+const mensagensDivertidas =
+[
+    "Por que os químicos são ótimos em resolver problemas? Porque eles têm todas as soluções!"
+    "Por que o desenvolvedor faliu? Porque ele usou todo o seu cache."
+    "Você já ouviu falar do cara que roubou o calendário? Ele pegou 12 meses!"
+    "O que ganha o melhor dentista do mundo? Uma pequena placa."
+    "Meus professores me disseram que eu nunca seria muito porque procrastino muito. Eu disse a eles: Esperem para ver!"
+    "Minha memória ficou tão ruim que realmente me fez perder o emprego. Ainda estou empregado. Só não consigo lembrar onde."
+    "Quando em uma candidatura a emprego perguntam quem deve ser notificado em caso de emergência, sempre escrevo: Um médico muito bom."
+];
+
+let botaoDivertido = document.getElementById("botaoDivertido");
+let mensagemDivertida = document.getElementById("mensagemDivertida");
+    //math.floor(x) retorna o menor número inteiro dentre o número "x"
+    //math.ramdom retorna um número psed inteirodentre o número "x"
+    const mensagemDivertida.textContent
